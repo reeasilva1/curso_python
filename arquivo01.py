58efd248e5044dd143196398ca149341091d7ff4
Renato Silva <renato.asilva@yahoo.com.br>
2022-01-15 11:56:08 -0300

    primeiro arquivo

---
+++ arquivo01.py
@@ -0,0 +1,70 @@
+from cgitb import text
+
+
+#print("Ola")
+
+inicial = "Aula "
+numeroInt = 1
+numeroText = "1"
+final = " de Python"
+
+texto = inicial + numeroText + final
+
+#print(texto)
+
+#interacao = input("Qual é o seu nome: ")
+
+#primeiro_numero = input("Digite o primeiro numero: ")
+#segundo_numero = input("Digite o segundo numero: ")
+#print(interacao)
+#print(primeiro_numero + segundo_numero)
+
+#primeiro_numero_int = int(primeiro_numero)
+#segundo_numero_int = int(segundo_numero)
+
+#print(primeiro_numero_int + segundo_numero_int)
+
+#print(inicial[0],inicial[2])
+
+texto_um = "COD-123914124198-INT"
+
+#print(texto_um[4:-4])
+
+str_elimina_primeiros = texto_um[4:]
+str_apenas_os_primeiros = texto_um[-4]
+str_elimina_ultimos = texto_um[:-4]
+str_conteudo_do_meio = texto_um[4:-4]
+
+text_cinco = "esse texto"
+text_seis = "essetexto"
+text_sete = "EsseTexto"
+
+#print(text_cinco.upper())
+#print(text_seis.lower())
+#print(text_cinco.capitalize())
+#print(text_sete.swapcase())
+#print(text_cinco.title()) 
+
+
+
+#Aspas triplas
+
+text_dois = """
+ Consigo
+ Fazer\n
+ coisa
+ assim """
+
+#print(text_dois)
+
+
+
+operadores = []
+
+notas_aluno = [1,2,3,4,6,7,8]
+
+notas_aluno.append(9)
+
+print(notas_aluno)
+
+
